3f6a612478223792fa7917412bece9b8e803f7b5
Harikrishna-AL <amenu4aie21034@am.students.amrita.edu>
2024-10-13 00:31:45 +0530

    matmul to mul

--- model.py
+++ model.py
@@ -204,11 +204,11 @@ class GPT(nn.Module):
         x = self.transformer.ln_f(x)
 
         # forward the future n tokens model
-        future_n_tokens = self.transformer.future_n_tokens(x).T
+        future_n_tokens = self.transformer.future_n_tokens(x)
         # add n future tokens to the ith token
-        print(future_n_tokens.shape)
-        print(x.shape)
-        x = x + torch.matmul(future_n_tokens, x)
+        # print(future_n_tokens.shape)
+        # print(x.shape)
+        x = x + future_n_tokens * x
 
 
         if targets is not None:
